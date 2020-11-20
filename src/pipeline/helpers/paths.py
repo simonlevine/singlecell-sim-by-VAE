cca92b85a6164be7d4f157649cb64f3dcf67090c
root <simon.levinegott@gmail.com>
2020-11-20 22:16:11 +0000

    fpath update

--- src/pipeline/helpers/paths.py
+++ src/pipeline/helpers/paths.py
@@ -3,8 +3,8 @@ from pathlib import Path
 INTERMEDIATE_DATA_DIR = Path("data/intermediate")
 ARTIFACTS_DATA_DIR = Path("data/artifacts")
 PARAMS_FP = Path("params.toml")
-PREPROC_DATA_FP = "data/intermediate/HCL_final_USE_preprocessed.h5ad"
-RAW_DATA_FP = "data/raw/HCL_final_USE.h5ad"
+PREPROC_DATA_FP = "data/intermediate/Single_cell_atlas_of_peripheral_immune_response_to_SARS_CoV_2_infection_preprocessed.h5ad"
+RAW_DATA_FP = "data/raw/Single_cell_atlas_of_peripheral_immune_response_to_SARS_CoV_2_infection.h5ad"
 
 DATA_SPLIT_FPS = [
     INTERMEDIATE_DATA_DIR / f"hcl_{split_type}_data.h5ad"
