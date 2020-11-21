389bda5c7ae6b7a1daed99ba7df1773697e4f786
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-11-21 08:43:42 -0800

    Update preprocess.py

--- src/pipeline/preprocess.py
+++ src/pipeline/preprocess.py
@@ -14,6 +14,7 @@ def main():
 
     logger.info('Computing QC metrics for ingested data...')
     filtered = filter(adata)
+
     normed = log_normalize(filtered)
     normed.write_h5ad(PREPROC_DATA_FP, compression="gzip")
  
@@ -21,28 +22,28 @@ def main():
 def filter(adata):
     logger.info(f'Filtering out datapoints based on number of genes and minimal presence...')
     logger.info('Setting lower bound on min number of genes to 200...')
-    # sc.pp.filter_cells(adata, min_genes=200)
+    sc.pp.filter_cells(adata, min_genes=200)
 
     logger.info('Setting lower bound on min number of cells with genes to 5...')
-    # sc.pp.filter_genes(adata, min_cells=5)
+    sc.pp.filter_genes(adata, min_cells=5)
 
     logger.info(f'Filtering out genes with counts < 2000...')
-    # adata = adata[adata.obs.n_counts > 2000, :]
+    adata = adata[adata.obs.n_counts > 2000, :]
     logger.info(f'Filtering out mitochondrial noise at 5% threshold...')
-    # adata = adata[adata.obs.pct_counts_mt < 5, :]
+    adata = adata[adata.obs.percent_counts_mt < 5, :]
 
     return adata
 
 def log_normalize(adata):
-    # sc.pp.normalize_total(adata, target_sum=1e4)
-    # sc.pp.log1p(adata)
-    # sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+    sc.pp.normalize_total(adata, target_sum=1e4)
+    sc.pp.log1p(adata)
+    sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
     logger.info('Identified highly variate genes. Filtering out...')
-    # adata = adata[:, adata.var.highly_variable]
+    adata = adata[:, adata.var.highly_variable]
     logger.info('Regressing out effects of percentage of mitochondrial genes expressed')
-    # sc.pp.regress_out(adata, ['percent_mt'])
+    sc.pp.regress_out(adata, ['percent_mt'])
     logger.info('Scaling the data to unit variance, excluding values exceeding standard dev of 10.')
-    # sc.pp.scale(adata)
+    sc.pp.scale(adata)
 
     return adata
 
