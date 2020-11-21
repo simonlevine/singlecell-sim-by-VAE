e71e6b9214d5108eebdbc23b3cef1f346e817787
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-11-21 11:33:44 -0800

    add

--- src/pipeline/preprocess.py
+++ src/pipeline/preprocess.py
@@ -7,6 +7,23 @@ from tqdm import tqdm
 from loguru import logger
 from helpers.paths import RAW_DATA_FP, DATA_SPLIT_FPS,PREPROC_DATA_FP
 
+
+
+"""Preprocessing recipes from the literature:
+
+sc.pp.filter_genes(adata, min_counts=1)         # only consider genes with more than 1 count
+sc.pp.normalize_per_cell(                       # normalize with total UMI count per cell
+     adata, key_n_counts='n_counts_all'
+)
+filter_result = sc.pp.filter_genes_dispersion(  # select highly-variable genes
+    adata.X, flavor='cell_ranger', n_top_genes=n_top_genes, log=False
+)
+adata = adata[:, filter_result.gene_subset]     # subset the genes
+sc.pp.normalize_per_cell(adata)                 # renormalize after filtering
+if log: sc.pp.log1p(adata)                      # log transform: adata.X = log(adata.X + 1)
+sc.pp.scale(adata)                              # scale to unit variance and shift to zero mean
+
+"""
 def main():
 
     adata = sc.read(RAW_DATA_FP,
@@ -14,39 +31,43 @@ def main():
 
     logger.info('Computing QC metrics for ingested data...')
     sc.pp.calculate_qc_metrics(adata, inplace=True)
-    filtered = filter(adata)
+    sc.pp.recipe_zheng17(adata, n_top_genes=1000, log=True, plot=False, copy=False)
+    adata.write(PREPROC_DATA_FP)
 
-    normed = log_normalize(filtered)
-    normed.write_h5ad(PREPROC_DATA_FP, compression="gzip")
+    # filtered = filter(adata)
+
+    # normed = log_normalize(filtered)
+    # normed.write(PREPROC_DATA_FP)
+    # normed.write_h5ad(PREPROC_DATA_FP, compression="gzip")
  
 
-def filter(adata):
-    logger.info(f'Filtering out datapoints based on number of genes and minimal presence...')
-    logger.info('Setting lower bound on min number of genes to 200...')
-    sc.pp.filter_cells(adata, min_genes=200) #takes awhile
+# def filter(adata):
+#     logger.info(f'Filtering out datapoints based on number of genes and minimal presence...')
+#     logger.info('Setting lower bound on min number of genes to 200...')
+#     sc.pp.filter_cells(adata, min_genes=200) #takes awhile
 
-    logger.info('Setting lower bound on min number of cells with genes to 5...')
-    sc.pp.filter_genes(adata, min_cells=5) #takes awhile
+#     logger.info('Setting lower bound on min number of cells with genes to 5...')
+#     sc.pp.filter_genes(adata, min_cells=5) #takes awhile
 
-    # logger.info(f'Filtering out genes with counts < 2000...')
-    # adata = adata[adata.obs.n_counts > 2000, :]
-    logger.info(f'Filtering out mitochondrial noise at 5% threshold...')
-    adata = adata[adata.obs.percent_counts_mt < 5, :]
+#     # logger.info(f'Filtering out genes with counts < 2000...')
+#     # adata = adata[adata.obs.n_counts > 2000, :]
+#     logger.info(f'Filtering out mitochondrial noise at 5% threshold...')
+#     adata = adata[adata.obs.percent_counts_mt < 5, :]
 
-    return adata
+#     return adata
 
-def log_normalize(adata):
-    sc.pp.normalize_total(adata, target_sum=1e4)
-    sc.pp.log1p(adata)
-    sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
-    logger.info('Identified highly variate genes. Filtering out...')
-    adata = adata[:, adata.var.highly_variable]
-    logger.info('Regressing out effects of percentage of mitochondrial genes expressed')
-    sc.pp.regress_out(adata, ['total_counts','percent_mt'])
-    logger.info('Scaling the data to unit variance, excluding values exceeding standard dev of 10.')
-    sc.pp.scale(adata,max_value=10)
+# def log_normalize(adata):
+#     sc.pp.normalize_total(adata, target_sum=1e4)
+#     sc.pp.log1p(adata)
+#     sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+#     logger.info('Identified highly variate genes. Filtering out...')
+#     adata = adata[:, adata.var.highly_variable]
+#     logger.info('Regressing out effects of percentage of mitochondrial genes expressed')
+#     sc.pp.regress_out(adata, ['total_counts','percent_mt'])
+#     logger.info('Scaling the data to unit variance, excluding values exceeding standard dev of 10.')
+#     sc.pp.scale(adata,max_value=10)
 
-    return adata
+#     return adata
 
 if __name__=='__main__':
     main()
