f262d72f28201c81f9f0e272769e530970c633a4
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-11-25 16:45:38 -0800

    Update main.py

--- src/PyTorch-Classifier/main.py
+++ src/PyTorch-Classifier/main.py
@@ -12,13 +12,13 @@ from pytorch_lightning.metrics.functional import accuracy
 
 from pipeline.datalib import load_single_cell_data
 
-
 def main():
 
     dm = load_single_cell_data()
     model = SingleCellClassifier()
     trainer = pl.Trainer(gpus = torch.cuda.device_count(), fast_dev_run = True)
     trainer.fit(model = model, datamodule= dm)
+
 
 class SingleCellClassifier(pl.LightningModule):
     
@@ -33,23 +33,56 @@ class SingleCellClassifier(pl.LightningModule):
         self.num_classes = 3
 
         # Define PyTorch model
-        self.model = nn.Sequential(
+        self.fc1 = nn.Sequential(
             nn.Linear(self.input_size, 4096),
             nn.ReLU(),
             nn.Dropout(0.1),
             nn.Linear(4096, 2048),
             nn.ReLU(),
             nn.Dropout(0.1),
-            nn.Linear(2048, 2048),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(2048, 2048),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(1024, self.num_classes)
         )
-    def forward(self, x):
-        x = self.model(x)
+        self.fc2 = nn.Sequential(
+            nn.Linear(2048, 2048),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(2048, 2048),
+            nn.ReLU(),
+            nn.Dropout(0.1))
+
+        self.fc3 = nn.Sequential(
+            nn.Linear(2048, 2048),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(2048, 2048),
+            nn.ReLU(),
+            nn.Dropout(0.1))
+
+        self.fc4 = nn.Sequential(
+            nn.Linear(2048, 2048),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(2048, 2048),
+            nn.ReLU(),
+            nn.Dropout(0.1))
+
+        self.output = nn.Linear(1024, self.num_classes)
+
+    def forward(self,x,skip_conn=False):
+        if skip_conn:
+            residual = x
+            x = self.fc1(x)
+            x = self.fc2(x)
+            x = self.fc3(x+residual)
+            x = self.fc4(x+residual)
+
+        else:
+            x = self.fc1(x)
+            x = self.fc2(x)
+            x = self.fc3(x)
+            x = self.fc4(x)
+            
+        x = self.output(x)
+        
         return F.log_softmax(x, dim=1)
 
     def training_step(self, batch, batch_idx):
