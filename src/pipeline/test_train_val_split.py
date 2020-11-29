45f4abbadbd6fcd4dbec42d326dab6704e9dda27
Jeremy Fisher <jeremyf@andrew.cmu.edu>
2020-11-29 11:30:59 -0500

    shuffle anndata

--- src/pipeline/test_train_val_split.py
+++ src/pipeline/test_train_val_split.py
@@ -1,6 +1,7 @@
 """create an 8:1:1 split for train, test and validation"""
 
 import anndata
+import random
 from tqdm import tqdm
 from loguru import logger
 from pipeline.helpers.paths import RAW_DATA_FP, DATA_SPLIT_FPS
@@ -10,9 +11,11 @@ logger.info("Please note this process takes about 10 minutes")
 covid_dataset = anndata.read_h5ad(RAW_DATA_FP, backed="r")
 
 n, _ = covid_dataset.shape
+idxs = random.sample(range(n), k=n)
 split_idx_1 = int(n * 0.8)
 split_idx_2 = int(n * 0.9)
 train_fp, test_fp, val_fp = DATA_SPLIT_FPS
+
 splits = [
     ("train", None, split_idx_1, train_fp),
     ("test", split_idx_1, split_idx_2, test_fp),
@@ -20,8 +23,6 @@ splits = [
 ]
 for split_type, i, j, outfp in tqdm(splits, unit="split"):
     logger.info("Extracting single cell {} data within [{},{})", split_type, i, j)
-    split = covid_dataset[slice(i,j), :]
+    split = covid_dataset[idxs[i:j]]
     split.write_h5ad(outfp, compression="gzip")
-    # bug workaround: after writing, we need to refresh the view
     covid_dataset = anndata.read_h5ad(RAW_DATA_FP, backed="r")
-
