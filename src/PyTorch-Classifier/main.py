ef9e55f695e9b5e49cfbdefce32a9a027a9e5f3d
Simon Levine-Gottreich <50503513+simonlevine@users.noreply.github.com>
2020-12-03 08:00:29 -0800

    Finish classifier (#14)
    * Update main.py
    
    * add metrics, concat classifier
    
    * Update dvc.yaml
    
    Co-authored-by: Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>

--- src/PyTorch-Classifier/main.py
+++ src/PyTorch-Classifier/main.py
@@ -8,16 +8,30 @@ from torch.nn import functional as F
 from torchvision.datasets import MNIST
 from torchvision import transforms
 import pytorch_lightning as pl
-from pytorch_lightning.metrics.functional import accuracy
+import pytorch_lightning.metrics.functional as metrics
+from pytorch_lightning import loggers as pl_loggers
 
-from pipeline.datalib import load_single_cell_data
+from pipeline.datalib import load_single_cell_data, load_combined_single_cell_data
 
 def main():
 
     dm = load_single_cell_data()
     model = SingleCellClassifier()
-    trainer = pl.Trainer(gpus = torch.cuda.device_count(), fast_dev_run = True)
+    tb_logger = pl_loggers.TensorBoardLogger('baseline_logs/')
+
+    trainer = pl.Trainer(gpus = torch.cuda.device_count(), fast_dev_run = True, logger=tb_logger)
     trainer.fit(model = model, datamodule= dm)
+    trainer.test(model, test_dataloaders=dm.test_dataloader())
+
+
+    dm = load_combined_single_cell_data()
+    model = SingleCellClassifier()
+    tb_logger = pl_loggers.TensorBoardLogger('simulated_logs/')
+
+    trainer = pl.Trainer(gpus = torch.cuda.device_count(), fast_dev_run = True, logger=tb_logger)
+    trainer.fit(model = model, datamodule= dm)
+    trainer.test(model, test_dataloaders=dm.test_dataloader())
+
 
 
 class SingleCellClassifier(pl.LightningModule):
@@ -38,24 +52,18 @@ class SingleCellClassifier(pl.LightningModule):
             nn.Linear(4096, 2048),
             nn.ReLU(),
             nn.Dropout(0.1),
-        )
-        self.fc2 = nn.Sequential(
             nn.Linear(2048, 2048),
             nn.ReLU(),
             nn.Dropout(0.1),
             nn.Linear(2048, 2048),
             nn.ReLU(),
-            nn.Dropout(0.1))
-
-        self.fc3 = nn.Sequential(
+            nn.Dropout(0.1),
             nn.Linear(2048, 2048),
             nn.ReLU(),
             nn.Dropout(0.1),
             nn.Linear(2048, 2048),
             nn.ReLU(),
-            nn.Dropout(0.1))
-
-        self.fc4 = nn.Sequential(
+            nn.Dropout(0.1),
             nn.Linear(2048, 2048),
             nn.ReLU(),
             nn.Dropout(0.1),
@@ -65,28 +73,16 @@ class SingleCellClassifier(pl.LightningModule):
 
         self.output = nn.Linear(1024, self.num_classes)
 
-    def forward(self,x,skip_conn=False):
-        if skip_conn:
-            residual = x
-            x = self.fc1(x)
-            x = self.fc2(x)
-            x = self.fc3(x+residual)
-            x = self.fc4(x+residual)
-
-        else:
-            x = self.fc1(x)
-            x = self.fc2(x)
-            x = self.fc3(x)
-            x = self.fc4(x)
-            
+    def forward(self,x):
+        x = self.fc1(x)
         x = self.output(x)
-        
         return F.log_softmax(x, dim=1)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
         logits = self(x)
         loss = F.nll_loss(logits, y)
+        self.log('train_loss', loss, prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -94,14 +90,37 @@ class SingleCellClassifier(pl.LightningModule):
         logits = self(x)
         loss = F.nll_loss(logits, y)
         preds = torch.argmax(logits, dim=1)
-        acc = accuracy(preds, y)
+        acc = metrics.accuracy(preds, y)
+        f1 = metrics.f1_score(preds,y)
+        recall = metrics.recall(preds,y)
+        precision=metrics.precision(preds,y)
 
         self.log('val_loss', loss, prog_bar=True)
         self.log('val_acc', acc, prog_bar=True)
-        return loss
+        self.log('val_f1',f1,prog_bar=True)
+        self.log('val_recall',recall,prog_bar=True)
+        self.log('val_precision',precision,prog_bar=True)
+        return {'val_loss':loss, 'val_acc': acc, 'val_f1':f1,'val_recall':recall,'val_precision':precision}
 
     def test_step(self, batch, batch_idx):
-        return self.validation_step(batch, batch_idx)
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+
+        acc = metrics.accuracy(preds, y)
+        f1 = metrics.f1_score(preds,y)
+        recall = metrics.recall(preds,y)
+        precision=metrics.precision(preds,y)
+
+        self.log('test_loss', loss, prog_bar=True)
+        self.log('test_acc', acc, prog_bar=True)
+        self.log('test_f1',f1,prog_bar=True)
+        self.log('test_recall',recall,prog_bar=True)
+        self.log('test_precision',precision,prog_bar=True)
+        
+        return {'test_loss':loss, 'test_acc': acc, 'test_f1':f1,'test_recall':recall,'test_precision':precision}
+        
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
